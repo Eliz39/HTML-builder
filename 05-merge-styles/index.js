d8f88fc53f5dec3a0c953da614ead3e7a3143185
Eliz39 <liza.nikitina9898@gmail.com>
2022-05-21 11:03:56 +0200

    fix: 5th solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,27 +8,27 @@ const pathStyle = path.join(__dirname, 'project-dist', 'bundle.css');
 const output = fs.createWriteStream(pathStyle);
 
 async function readFolder() {
-    let files;
-    try {
-        files = await readdir(pathFolder);
+  let files;
+  try {
+    files = await readdir(pathFolder);
 
-                files.forEach(function(file) {
-                    const pathFile = path.join(__dirname, 'styles', `${file}`);
+    files.forEach(function(file) {
+      const pathFile = path.join(__dirname, 'styles', `${file}`);
 
-                    const extension = path.extname(pathFile).split('.').join('');
-                    fs.stat(pathFile, (err, file) => {
-                        if (err) throw err;
-                        if (file.isFile() && extension == 'css') {
-                            const readableStream = fs.createReadStream(pathFile, 'utf-8');
-                            readableStream.on("data", (fileContent) => {
-                                output.write(fileContent);
-                               });
-                        }
-                    })
-                })
+      const extension = path.extname(pathFile).split('.').join('');
+      fs.stat(pathFile, (err, file) => {
+        if (err) throw err;
+        if (file.isFile() && extension == 'css') {
+          const readableStream = fs.createReadStream(pathFile, 'utf-8');
+          readableStream.on('data', (fileContent) => {
+            output.write(fileContent);
+          });
         }
-         catch (err) {
-        console.error(err);
-      }
-    }
-readFolder()
+      });
+    });
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+readFolder();
